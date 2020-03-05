9a3706f80bdd7ccb8ab850ac41197738fe3638cf
Ranee Joshi <22663327@student.uwa.edu.au>
2020-03-05 12:06:10 +0800

    first working version

--- notebooks/dh2l_config.py
+++ notebooks/dh2l_config.py
@@ -11,24 +11,32 @@ dhgeologyattr_file=wamex_path+'dhgeologyattr.csv'
 #Thesauri
 rl_maxdepth_dic_file= wamex_path+'rl_maxdepth_dic.csv'
 survey_dic_file=wamex_path+'survey_dic.csv'
-dic_attr_val_lithology_filter_file=wamex_path+'dic_attr_val_lithology_filter.csv'
-litho_dic_1_file=wamex_path+'litho_dic_1,csv'
+dic_attr_col_lithology_file=wamex_path+'dic_att_col_lithology.csv'
+dic_attr_val_lithology_file=wamex_path+'dic_attr_val_lithology_filter.csv'
 cleanup_lithology_file=wamex_path+'cleanup_lithology.csv'
-dic_att_col_lithology_file=wamex_path+'dic_att_col_lithology.csv'
+litho_dic_file=wamex_path+'litho_dic_1.csv'
 CET_hierarchy_dico_file=wamex_path+'hierarchy_dico.csv'
 
 #ExportFiles
 export_path='../data/export/'
 DB_Collar_Export=export_path+'DB_Collar_Export.csv'
 DB_Survey_Export=export_path+'DB_Survey_Export.csv'
+DB_Survey_Export_Calc=export_path+'DB_Survey_Export_Calc.csv'
 CET_Litho=export_path+'CET_Litho.csv'
 DB_Lithology_Export=export_path+'DB_Lithology_Export.csv'
 DB_Lithology_Export_Backup=export_path+'DB_Lithology_Export_Backup.csv'
-DH_Lithology_Upscaled=export_path+'DH_Lithology_Upscaled.csv'
+DB_Lithology_Upscaled=export_path+'DB_Lithology_Upscaled.csv'
+DB_Lithology_Export_Calc=export_path+'DB_Lithology_Export_Calc.csv'
+DB_Lithology_Export_VTK=export_path+'DB_Lithology_Export.vtp'
+
 
 #Shapefiles
 shapefile_path='../data/shapefile/'
 geology=shapefile_path+'500K_interpgeol16_yalgoo_singleton.shp'
+
+#Projections
+src_crs = {'init': 'EPSG:4326'}  # coordinate reference system for imported dtms (geodetic lat/long WGS84)
+dst_crs = {'init': 'EPSG:28350'} # coordinate system for data
 
 print('Default parameters loaded from dh2l_config.py:')
 with open('../notebooks/dh2l_config.py', 'r') as myfile:
